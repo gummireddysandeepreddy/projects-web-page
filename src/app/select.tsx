6bd3aee37326dc74a7963f99347bebbc247de5fe
gummireddysandeepreddy <gummireddysandeepreddy@gmail.com>
2024-08-17 13:10:56 +0530

    Dropdown menu added

---
+++ src/app/select.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { year, faculty } from "@/lib/details";
+import { useState } from "react";
+
+export default function Select() {
+	const [value, setValue] = useState(year[0]);
+	const [facultyList, setFacultyList] = useState(faculty[value]);
+	const [facultyValue, setFacultyValue] = useState(faculty[value][0]);
+	const handleValue = (event:any) => {
+		setValue(event.target.value);
+		setFacultyList(faculty[event.target.value]);
+	};
+    return (
+        <div>
+			<form className="flex flex-row gap-4">
+				<label className="text-2xl">Faculty Details</label>
+				<select value={value} onChange={handleValue}>
+				{year.map((detail) => (
+					<option key={detail} value={detail}>
+						{detail}
+					</option>
+				))}
+				</select>
+				<select value={facultyValue} onChange={(event) => setFacultyValue(event.target.value)}>
+				{facultyList.map((detail) => (
+					<option key={detail} value={detail}>
+						{detail}
+					</option>
+				))}
+				</select>
+			</form>
+		</div>
+    );
+}
